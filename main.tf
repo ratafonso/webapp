e341179705741ee2f558f758d86a3807c689c9d5
ratafonso <ratafonso@gmail.com>
2021-07-05 13:59:08 -0300

    Association Route Table Created

--- main.tf
+++ main.tf
@@ -7,7 +7,7 @@ resource "aws_vpc" "VPC-SERASA" {
   cidr_block           = "10.0.0.0/16"
   enable_dns_hostnames = true
   tags = {
-    "name" = "VPC-SERASa"
+    "name" = "VPC-SERASA"
   }
 }
 
@@ -33,7 +33,7 @@ resource "aws_subnet" "PublicSubnetB" {
   }
 }
 
-resource "aws_eip" "NIG" {
+resource "aws_eip" "EIP" {
   vpc        = true
   depends_on = [aws_internet_gateway.IGW]
   tags = {
@@ -49,7 +49,7 @@ resource "aws_internet_gateway" "IGW" {
 }
 
 resource "aws_nat_gateway" "NAT_GW" {
-  allocation_id = aws_eip.NAT.id
+  allocation_id = aws_eip.EIP.id
   subnet_id     = aws_subnet.PrivateSubnetA.id
   depends_on = [aws_internet_gateway.IGW]
   tags = {
@@ -71,55 +71,3 @@ resource "aws_route_table_association" "assoc" {
   subnet_id      = aws_subnet.PrivateSubnetA.id
   route_table_id = aws_route_table.Rotas.id
 }
-
-#resource "aws_elb" "PublicALB" {
-#    name = "PublicALB"
-#    availability_zones = ["us-east-1a","us-east-1b"]
-#    listener {
-#      instance_port = 8000
-#      instance_protocol = "http"
-#      lb_port = 80
-#      lb_protocol = "http"
-#    }
-#    health_check {
-#      healthy_threshold = 2
-#      unhealthy_threshold = 2
-#      timeout = 3
-#      target = "HTTP:8000/"
-#      interval = 30
-#    }
-#    instances = [aws_instance.WebServerFE.id]
-#    cross_zone_load_balancing = true
-#    idle_timeout = 400
-#    connection_draining = true
-#    connection_draining_timeout = 400
-#    tags = {
-#      "name" = "ALB_FE"
-#    }
-#}
-
-#resource "aws_elb" "InternalALB" {
-#    name = "InternalALB"
-#    availability_zones = ["us-east-1a","us-east-1b"]
-#    listener {
-#      instance_port = 8000
-#      instance_protocol = "http"
-#      lb_port = 8080
-#      lb_protocol = "http"
-#    }
-#    health_check {
-#      healthy_threshold = 2
-#      unhealthy_threshold = 2
-#      timeout = 3
-#      target = "HTTP:8000/"
-#      interval = 30
-#    }
- #   instances = [aws_instance.WebServerBE.id]
- #   cross_zone_load_balancing = true
- #   idle_timeout = 400
- #   connection_draining = true
- #   connection_draining_timeout = 400
- #   tags = {
- #     "name" = "ALB_BE"
- #   }
-#}
